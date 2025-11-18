0361aaedc65981819a45fd62fe1e7f9420c13fae
mgreen <mgreen@bournemouth.ac.uk>
2025-11-18 17:31:43 +0000

    trying to implement latin square in spreadsheet

--- alej_colour_self_friend_other.py
+++ alej_colour_self_friend_other.py
@@ -135,7 +135,7 @@ def setupData(expInfo, dataDir=None):
     thisExp = data.ExperimentHandler(
         name=expName, version=expVersion,
         extraInfo=expInfo, runtimeInfo=None,
-        originPath='C:\\Users\\Matt\\gits\\alej_colour_self_friend_other\\alej_colour_self_friend_other_lastrun.py',
+        originPath='C:\\Users\\Matt\\gits\\alej_colour_self_friend_other\\alej_colour_self_friend_other.py',
         savePickle=True, saveWideText=True,
         dataFileName=dataDir + os.sep + filename, sortColumns='time'
     )
